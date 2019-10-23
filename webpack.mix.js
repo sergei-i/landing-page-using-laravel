1d295bee3578b4a56754b8e8b97a052df4e15b31
Sergei <1161441@gmail.com>
2019-10-23 12:08:07 +0200

    Added main view

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,28 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.styles([
+        'resources/assets/css/bootstrap.min.css',
+        'resources/assets/css/style.css',
+        'resources/assets/css/font-awesome.css',
+        'resources/assets/css/animate.css'
+    ],
+    'public/assets/css/main.css');
+
+mix.scripts([
+    'resources/assets/js/respond-1.1.0.min.js',
+    'resources/assets/js/html5shiv.js',
+    'resources/assets/js/html5element.js',
+    'resources/assets/js/jquery-1.11.0.min.js',
+    'resources/assets/js/bootstrap.min.js',
+    'resources/assets/js/jquery-scrolltofixed.js',
+    'resources/assets/js/jquery.nav.js',
+    'resources/assets/js/jquery.easing.1.3.js',
+    'resources/assets/js/jquery.isotope.js',
+    'resources/assets/js/wow.js',
+    'resources/assets/js/custom.js'
+], 'public/assets/js/main.js');
+
+mix.copy('resources/assets/fonts', 'public/assets/fonts');
+mix.copy('resources/assets/img', 'public/assets/img');
+mix.copy('resources/assets/favicon.png', 'public/assets/favicon.png');
